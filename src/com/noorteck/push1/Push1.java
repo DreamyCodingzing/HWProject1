a0dd5c96d93789fe2bc1b049cadeda63c9582bdf
Running Bird <Running Bird@DESKTOP-GE19MGH>
2023-04-10 17:13:18 -0400

    comment

---
+++ src/com/noorteck/push1/Push1.java
@@ -0,0 +1,14 @@
+package com.noorteck.push1;
+
+public class Push1 {
+
+	public static void main(String[] args) {
+	
+	}
+
+	public void sysMessage() {
+		System.out.println("push1");
+		System.out.println("yaaaaaaa");
+	}
+
+}
